9f478b295f832dd42d77279112bcc2fed84665e2
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-06-04 01:29:27 +0530

    PWA done

--- client/build/worker.js
+++ client/build/worker.js
@@ -1,48 +1,88 @@
+
+// var urlsToCache = [
+//   '/',
+// //You can add more routes if you wish.Mine is a simple one therefore am caching '/' only
+// ];
 var CACHE_NAME = 'justgist';
-var urlsToCache = [
+const toCache = [
   '/',
-//You can add more routes if you wish.Mine is a simple one therefore am caching '/' only
+  '/index.html',
 ];
 
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
+// // Install a service worker
+// self.addEventListener('install', event => {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+// // Cache and return requests
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
+
+// // Update a service worker
+// self.addEventListener('activate', event => {
+//   var cacheWhitelist = ['justgist'];
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cacheName => {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(toCache)
       })
-  );
-});
+      .then(self.skipWaiting())
+  )
+})
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['justgist'];
+    fetch(event.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match(event.request)
+          })
+      })
+  )
+})
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
           }
-        })
-      );
-    })
-  );
-});
+        }))
+      })
+      .then(() => self.clients.claim())
+  )
+})
