aa4b1bb043585c5b030304da0085855eafaa01ca
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2021-06-04 15:12:46 +0530

    API Keys Added

--- server/index.js
+++ server/index.js
@@ -22,17 +22,17 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 let query="";
 app.get('/news', function(req, res, next) {
-    request('https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey=06f0790a0cb240d9afc49242b64fbe75', options, function(error, response, body) {
+    request('https://newsapi.org/v2/top-headlines?country=in&pageSize=100&apiKey=f9fb08a6fe4b409584ed26f296944b4b', options, function(error, response, body) {
         res.send(body)
     });
 });
 app.get('/businessnews', function(req, res, next) {
-  request('https://newsapi.org/v2/top-headlines?country=in&category=business&pageSize=100&apiKey=06f0790a0cb240d9afc49242b64fbe75', options, function(error, response, body) {
+  request('https://newsapi.org/v2/top-headlines?country=in&category=business&pageSize=100&apiKey=375a9b5307274bba94cf86101d06c030', options, function(error, response, body) {
       res.send(body)
   });
 });
 app.get('/entertainmentnews', function(req, res, next) {
-  request('https://newsapi.org/v2/top-headlines?country=in&category=entertainment&pageSize=100&apiKey=06f0790a0cb240d9afc49242b64fbe75', options, function(error, response, body) {
+  request('https://newsapi.org/v2/top-headlines?country=in&category=entertainment&pageSize=100&apiKey=64bdafeb6c6d4018829dfd5ea76bfc9c', options, function(error, response, body) {
       res.send(body)
   });
 });
@@ -42,17 +42,17 @@ app.get('/healthnews', function(req, res, next) {
   });
 });
 app.get('/sciencenews', function(req, res, next) {
-  request('https://newsapi.org/v2/top-headlines?country=in&category=science&pageSize=100&apiKey=06f0790a0cb240d9afc49242b64fbe75', options, function(error, response, body) {
+  request('https://newsapi.org/v2/top-headlines?country=in&category=science&pageSize=100&apiKey=f9fb08a6fe4b409584ed26f296944b4b', options, function(error, response, body) {
       res.send(body)
   });
 });
 app.get('/sportsnews', function(req, res, next) {
-  request('https://newsapi.org/v2/top-headlines?country=in&category=sports&pageSize=100&apiKey=06f0790a0cb240d9afc49242b64fbe75', options, function(error, response, body) {
+  request('https://newsapi.org/v2/top-headlines?country=in&category=sports&pageSize=100&apiKey=375a9b5307274bba94cf86101d06c030', options, function(error, response, body) {
       res.send(body)
   });
 });
 app.get('/technologynews', function(req, res, next) {
-  request('https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=100&apiKey=06f0790a0cb240d9afc49242b64fbe75', options, function(error, response, body) {
+  request('https://newsapi.org/v2/top-headlines?country=in&category=technology&pageSize=100&apiKey=64bdafeb6c6d4018829dfd5ea76bfc9c', options, function(error, response, body) {
       res.send(body)
   });
 });
